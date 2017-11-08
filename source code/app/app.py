f15c501741ee115c5a6bb17ca69cb46d8222807d
Cecilia Shi <xs41@duke.edu>
2017-11-08 00:35:13 -0500

    add pages

--- source code/app/app.py
+++ source code/app/app.py
@@ -5,7 +5,6 @@ import pandas as pd
 ## local scripts
 from Ploty.kw import kill_wound
 from Ploty.freq import freq
-from Ploty.plot_victim import plot_victim, victim_type 
 
 
 app = Flask(__name__)
@@ -26,18 +25,52 @@ def visual():
 
 @app.route('/world-map/')
 def world_map():
-	code = pd.read_csv("Ploty/code_correct.csv")
-	df = pd.read_csv("Ploty/freq.csv")
-	iframe = freq(df,code)
-	#df = pd.read_csv("Ploty/kw.csv")
-	#iframe = kill_wound(df,code)
-	#iframe = "https://plot.ly/~KimJin/0/550/550"
-	victim = plot_victim(victim_type)
-	return render_template("world-map.html", iframe = iframe, victim = victim)
+		code = pd.read_csv("Ploty/code_correct.csv")
+		df = pd.read_csv("Ploty/freq.csv")
+		iframe = freq(df,code)
+		#df = pd.read_csv("Ploty/kw.csv")
+		#iframe = kill_wound(df,code)
+		#iframe = "https://plot.ly/~KimJin/0/550/550"
+	return render_template("world-map.html", iframe = iframe)
+
+@app.route('/attack-type/')
+def attack_type():
+		code = pd.read_csv("Ploty/code_correct.csv")
+		df = pd.read_csv("Ploty/freq.csv")
+		iframe = freq(df,code)
+		#df = pd.read_csv("Ploty/kw.csv")
+		#iframe = kill_wound(df,code)
+		#iframe = "https://plot.ly/~KimJin/0/550/550"
+	return render_template("attack-type.html", iframe = iframe)
+
+@app.route('/victim-type/')
+def attack_type():
+		code = pd.read_csv("Ploty/code_correct.csv")
+		df = pd.read_csv("Ploty/freq.csv")
+		iframe = freq(df,code)
+		#df = pd.read_csv("Ploty/kw.csv")
+		#iframe = kill_wound(df,code)
+		#iframe = "https://plot.ly/~KimJin/0/550/550"
+	return render_template("victim-type.html", iframe = iframe)
+
+@app.route('/weapon-type/')
+def attack_type():
+		code = pd.read_csv("Ploty/code_correct.csv")
+		df = pd.read_csv("Ploty/freq.csv")
+		iframe = freq(df,code)
+		#df = pd.read_csv("Ploty/kw.csv")
+		#iframe = kill_wound(df,code)
+		#iframe = "https://plot.ly/~KimJin/0/550/550"
+	return render_template("weapon-type.html", iframe = iframe)
 
 @app.route('/comments/')
 def comments():
 	return render_template("comments.html")
+
+@app.route("/test" , methods=['GET', 'POST'])
+def test():
+    select = request.form.get('type')
+    return(str(select))
 
 if __name__ == "__main__":
     app.run()
