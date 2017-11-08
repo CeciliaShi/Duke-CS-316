589752f2dc0228643ad695a2a763be562d630f03
Cecilia Shi <xs41@duke.edu>
2017-11-08 00:30:38 -0500

    add more templates

--- source code/app/app.py
+++ source code/app/app.py
@@ -33,6 +33,36 @@ def world_map():
 		#iframe = "https://plot.ly/~KimJin/0/550/550"
 	return render_template("world-map.html", iframe = iframe)
 
+@app.route('/attack-type/')
+def attack_type():
+		code = pd.read_csv("Ploty/code_correct.csv")
+		df = pd.read_csv("Ploty/freq.csv")
+		iframe = freq(df,code)
+		#df = pd.read_csv("Ploty/kw.csv")
+		#iframe = kill_wound(df,code)
+		#iframe = "https://plot.ly/~KimJin/0/550/550"
+	return render_template("attack-type.html", iframe = iframe)
+
+@app.route('/victim-type/')
+def attack_type():
+		code = pd.read_csv("Ploty/code_correct.csv")
+		df = pd.read_csv("Ploty/freq.csv")
+		iframe = freq(df,code)
+		#df = pd.read_csv("Ploty/kw.csv")
+		#iframe = kill_wound(df,code)
+		#iframe = "https://plot.ly/~KimJin/0/550/550"
+	return render_template("victim-type.html", iframe = iframe)
+
+@app.route('/weapon-type/')
+def attack_type():
+		code = pd.read_csv("Ploty/code_correct.csv")
+		df = pd.read_csv("Ploty/freq.csv")
+		iframe = freq(df,code)
+		#df = pd.read_csv("Ploty/kw.csv")
+		#iframe = kill_wound(df,code)
+		#iframe = "https://plot.ly/~KimJin/0/550/550"
+	return render_template("weapon-type.html", iframe = iframe)
+
 @app.route('/comments/')
 def comments():
 	return render_template("comments.html")
