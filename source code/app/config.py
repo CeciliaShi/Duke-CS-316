fb33b471aa4b40dc1848cedfcc86f8ba07558b1e
Cecilia Shi <xs41@duke.edu>
2017-11-08 23:05:32 -0500

    edit config.py!

--- source code/app/config.py
+++ source code/app/config.py
@@ -1,4 +1,6 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://shixilin94:dbpasswd@localhost/beers'
+import os
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}:dbpasswd@localhost/gtd'.fomrat(os.getusername())
 SQLALCHEMY_ECHO = True
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
