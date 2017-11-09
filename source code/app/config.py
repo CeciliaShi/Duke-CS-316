405cf05b8cc7fe89eae6f248170951dd7b4b63db
Cecilia Shi <xs41@duke.edu>
2017-11-08 23:11:33 -0500

    edit config.py

--- source code/app/config.py
+++ source code/app/config.py
@@ -1,6 +1,7 @@
 import os
+import pwd
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://{}:dbpasswd@localhost/gtd'.format(os.getusername())
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}:dbpasswd@localhost/gtd'.format(pwd.getpwuid(os.getuid())[0])
 SQLALCHEMY_ECHO = True
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
