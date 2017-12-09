2c5e125239ca36297b4b8da11f1f620ce3c718a5
Cecilia Shi <xs41@duke.edu>
2017-12-08 19:36:03 -0500

    attempt 6

--- source code/app/Ploty/attack_info.py
+++ source code/app/Ploty/attack_info.py
@@ -22,39 +22,42 @@ def attack_info(df):
     print('Hello!')
     print(df.shape)
     df.head()
-    intn_y = round(float(np.float64(((df['international'] == 1).sum()/(df['international'] != -9).sum())*100)),2)
-    prop_y = round(float(np.float64(((df['property_damage'] == 1).sum()/(df['property_damage'] != -9).sum())*100)),2)
-    suicide_y = round(float(np.float64(((df['suicide_attack'] == 1).sum()/(df['suicide_attack'] != -9).sum())*100)),2)
-    succuss_y = round(float(np.float64(((df['succussful_attack'] == 1).sum()/(df['succussful_attack'] != -9).sum())))*100,2)
+    if df.shape[0] == 0:
+        plot_url = "https://plot.ly/~xingyu/6/550/550"
+    else:
+        intn_y = round(float(np.float64(((df['international'] == 1).sum()/(df['international'] != -9).sum())*100)),2)
+        prop_y = round(float(np.float64(((df['property_damage'] == 1).sum()/(df['property_damage'] != -9).sum())*100)),2)
+        suicide_y = round(float(np.float64(((df['suicide_attack'] == 1).sum()/(df['suicide_attack'] != -9).sum())*100)),2)
+        succuss_y = round(float(np.float64(((df['succussful_attack'] == 1).sum()/(df['succussful_attack'] != -9).sum())))*100,2)
+        
+        intn_n = 100 - intn_y
+        prop_n = 100 - prop_y
+        suicide_n =100 - suicide_y
+        succuss_n =100 - succuss_y
+        
+        trace1 = go.Bar(
+        x=['International', 'Property Damage', 'Suicide Attack', 'Successful Attack'],
+        y=[intn_y,prop_y,suicide_y,succuss_y],
+        name='Yes',
+        marker = dict(color = '#1883B2',),
+        opacity = 0.8
+        )
+        trace2 = go.Bar(
+        x=['International', 'Property Damage', 'Suicide Attack','Successful Attack'],
+        y=[intn_n,prop_n,suicide_n,succuss_n],
+        name='No',
+        marker = dict(color = '#A4E3FF',),
+        opacity = 0.8
+        )
     
-    intn_n = 100 - intn_y
-    prop_n = 100 - prop_y
-    suicide_n =100 - suicide_y
-    succuss_n =100 - succuss_y
+        data = [trace1, trace2]
+        layout = go.Layout(
+            title = 'Attack Distributions in Percentage',
+            barmode='group'
+        )
     
-    trace1 = go.Bar(
-    x=['International', 'Property Damage', 'Suicide Attack', 'Successful Attack'],
-    y=[intn_y,prop_y,suicide_y,succuss_y],
-    name='Yes',
-    marker = dict(color = '#1883B2',),
-    opacity = 0.8
-    )
-    trace2 = go.Bar(
-    x=['International', 'Property Damage', 'Suicide Attack','Successful Attack'],
-    y=[intn_n,prop_n,suicide_n,succuss_n],
-    name='No',
-    marker = dict(color = '#A4E3FF',),
-    opacity = 0.8
-    )
-
-    data = [trace1, trace2]
-    layout = go.Layout(
-        title = 'Attack Distributions in Percentage',
-        barmode='group'
-    )
-
-    fig = go.Figure(data=data, layout=layout)
-    plot_url = py.plot(fig, filename='attacks_info',auto_open = False)
+        fig = go.Figure(data=data, layout=layout)
+        plot_url = py.plot(fig, filename='attacks_info',auto_open = False)
     return plot_url
         
         
