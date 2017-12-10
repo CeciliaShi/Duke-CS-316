6d1e4444495a5109a5a33935317a0f7c17fc499d
XingyuYan <xingyunku@gmail.com>
2017-12-09 19:20:19 -0500

    edit

--- source code/app/Ploty/worldmap_freq.py
+++ source code/app/Ploty/worldmap_freq.py
@@ -42,7 +42,7 @@ def freq(df, code):
     )
 
     fig = dict( data=data, layout=layout )
-    plot_url = py.plot( fig, validate=False, filename='d3-world-map',auto_open = False)
+    plot_url = py.plot( fig, validate=False, filename='d3-world-map',auto_open = False, sharing = "secret")
     #iframe_string = '<iframe id="igraph" style="border:none" src="'  
     #iframe_string = iframe_string+plot_url+'/550/550" width="100%" height="700"></iframe>'
     
